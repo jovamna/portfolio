7d044a2abd6e383badf2fff895520939d9af8137
“jovamna” <jovalidia.medina@gmail.com>
2023-12-14 20:42:59 +0100

    vite cambio

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react(),],
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/frontend/' : '/',
   build: {
     minify: 'terser',
     terserOptions: {
