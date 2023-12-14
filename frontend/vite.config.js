4b0c4f06be4b0ce5a6ad7a428085d97bdef561e9
“jovamna” <jovalidia.medina@gmail.com>
2023-12-14 21:17:04 +0100

    assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react(),],
-  base: process.env.NODE_ENV === 'production' ? '/frontend/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/assets/' : '/',
   build: {
     minify: 'terser',
     terserOptions: {
